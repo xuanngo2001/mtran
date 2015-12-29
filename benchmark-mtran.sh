c3ae29c760dccd8eba7405290d256a0cae15f17c
limelime <limelime@users.noreply.github.com>
2015-12-29 14:03:45 -0500

    Add test data statistics and add separator(;) to easy later processing.

--- benchmark-mtran.sh
+++ benchmark-mtran.sh
@@ -17,6 +17,12 @@ DEST_DIR=$(readlink -ev "${DEST_DIR}")
 # Stop if source and destination directory are the same.
 if [ "${SRC_DIR}" = "${DEST_DIR}" ]; then echo "Error: Source and destination directory are the same. Aborted!"; exit 1; fi;
 
+# Get source directory(test data directory) statistic.
+SRC_SIZE=$(du -chs "${SRC_DIR}" | tail -n 1 | cut -f 1)
+SRC_FILES=$(find "${SRC_DIR}" -type f | wc -l)
+SRC_DIRS=$(find "${SRC_DIR}" -type d | wc -l)
+SRC_STATS="Size=${SRC_SIZE}, files=${SRC_FILES}, directories=${SRC_DIRS}"
+echo "Test data: ${SRC_STATS}"
 
 DATE_STRING=$(date +"%Y-%m-%d_%0k.%M.%S")
 COPY_CMDS=( cp tar tarbuffer rsync )
@@ -24,8 +30,6 @@ MASTER_LOG=excution-time-mtran.log
 for COPY_CMD in "${COPY_CMDS[@]}"
 do
 
-  echo "Time ${COPY_CMD} ..."
-  
   # Create temporary test directory.
   TEST_DIR="${DEST_DIR}/${DATE_STRING}-${COPY_CMD}-${RANDOM}"
   mkdir -p "${TEST_DIR}"
@@ -35,12 +39,10 @@ do
   
   # Time the execution.
   echo "time ./mtran ${COPY_CMD} ${SRC_DIR} ${TEST_DIR} ..."
-  #time ./mtran ${COPY_CMD} "${SRC_DIR}" "${TEST_DIR}" >> "${TMP_LOG}"
-  #{ time echo "${DATE_STRING} ${COPY_CMD}"; } 2>> "${TMP_LOG}"
   { time ./mtran ${COPY_CMD} "${SRC_DIR}" "${TEST_DIR}"; } 2>> "${TMP_LOG}"
   
-  # Log execution time.
-  echo -n "${DATE_STRING} ${COPY_CMD} " >> "${MASTER_LOG}"
+  # Log execution time. Separator=;
+  echo -n "${DATE_STRING}; ${SRC_STATS}; ${COPY_CMD}; " >> "${MASTER_LOG}"
   cat "${TMP_LOG}" | tr '\r\n' ' ' >> "${MASTER_LOG}"
   echo "" >> "${MASTER_LOG}"
   
