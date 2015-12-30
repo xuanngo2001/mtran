3bddf0a87a2df99c8644ebe5c4e4d54f387e535e
limelime <limelime@users.noreply.github.com>
2015-12-30 15:24:10 -0500

    Delete the whole line instead of replacing it with blank.

--- benchmark-graph.sh
+++ benchmark-graph.sh
@@ -33,7 +33,7 @@ done
 
 GNUPLOT_PG=benchmark-graph-gnuplot.pg
 # Delete last plot command.
-sed -i 's/^plot .*//' "${GNUPLOT_PG}"
+sed -i '/^plot .*/d' "${GNUPLOT_PG}"
 # Delete the last comma(,) and add plot command.
 echo "${PLOT_CMD}" | sed 's/,$//' >> "${GNUPLOT_PG}"
 
