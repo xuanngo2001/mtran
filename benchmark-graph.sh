e1ee31150eb2b850e6b451337414a3a9e19f9bc7
limelime <limelime@users.noreply.github.com>
2015-12-30 17:47:20 -0500

    Graph runtime based on dataset instead of time based.

--- benchmark-graph.sh
+++ benchmark-graph.sh
@@ -28,14 +28,26 @@ do
     COPY_CMD_LOG=$(readlink -ev "${COPY_CMD_LOG}")
     
   # Build plot commands. 
-  PLOT_CMD="${PLOT_CMD} \"${COPY_CMD_LOG}\" using 1:5 title \"${COPY_CMD}\","
+  #PLOT_CMD="${PLOT_CMD} \"${COPY_CMD_LOG}\" using 1:5 title \"${COPY_CMD}\","  # Time based xtic.
+  PLOT_CMD="${PLOT_CMD} \"${COPY_CMD_LOG}\" using 0:5:xtic(3) title \"${COPY_CMD}\","
+  
 done
 
 GNUPLOT_PG=benchmark-graph-gnuplot.pg
-# Delete last plot command.
-sed -i '/^plot .*/d' "${GNUPLOT_PG}"
-# Delete the last comma(,) and add plot command.
-echo "${PLOT_CMD}" | sed 's/,$//' >> "${GNUPLOT_PG}"
+
+# Processing xtics
+  XTICS_CMD="set xtics"
+	# Ignore comment | Get date and time | sort | uniq | number each line | Swap col1 and col2 | Replace newline with comma | Delete last comma
+	XLABEL_REDEFINED=$(grep -v '^#'  "${EXE_TIME_LOG}" | cut -d ';' -f 1 | sort | uniq | grep -n '^' | awk -F":" '{ print "\"" $2 "\" " $1}' | tr '\n' ', ' | sed 's/,$//')
+  XTICS_CMD="${XTICS_CMD} (${XLABEL_REDEFINED})"
+  sed -i '/^set xtics (/d' "${GNUPLOT_PG}"
+  echo "${XTICS_CMD}" >> "${GNUPLOT_PG}"
+  
+# Processing plot command.
+	# Delete last plot command.
+	sed -i '/^plot .*/d' "${GNUPLOT_PG}"
+	# Delete the last comma(,) and add plot command.
+	echo "${PLOT_CMD}" | sed 's/,$//' >> "${GNUPLOT_PG}"
 
 # Execute gnuplot.
 ./benchmark-graph-gnuplot.pg
