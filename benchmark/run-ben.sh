f7f94be8df998bf1c55ba88b490318693c0117f9
limelime <limelime@users.noreply.github.com>
2015-12-31 09:33:31 -0500

    Shorten test case description.

--- benchmark/run-ben.sh
+++ benchmark/run-ben.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk but different partition"
+./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk, diff partition"
 ./benchmark-graph.sh
