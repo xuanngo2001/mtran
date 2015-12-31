87fb6dd81f582610974e2554af983910750d5fd4
limelime <limelime@users.noreply.github.com>
2015-12-31 15:32:17 -0500

    Different test case run.

--- benchmark/run-ben.sh
+++ benchmark/run-ben.sh
@@ -4,41 +4,41 @@ set -e
 LOGFILE_DIR=./tmp-working
 
 # 257M
-#./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
+#./benchmark-mtran.sh /media/master/apps/               /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
 
 # 500M
-#./benchmark-mtran.sh /media/master/mywiki_deprecated/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
+#./benchmark-mtran.sh /media/master/mywiki_deprecated/  /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
 
 # 803M
-#./benchmark-mtran.sh /media/master/github/opwr /media/imdb/ "Same disk: VM part. to VM part."
+#./benchmark-mtran.sh /media/master/github/opwr         /media/imdb/                "Same HD: VM part. to VM part."
 
 # 
-#./benchmark-mtran.sh /media/master/github/opwr /media/master/github/fedca_proqc/ "Same disk: VM part. to VM part."
+#./benchmark-mtran.sh /media/master/github/fedca_proqc/ /media/imdb/                "Same HD: VM part. to VM part."
 
-./benchmark-mtran.sh /media/master/github/opwr /media/imdb/ "Same disk: VM part. to VM part."
-./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
-./benchmark-mtran.sh /media/master/mywiki_deprecated/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
-./benchmark-mtran.sh /media/master/github/opwr /media/master/github/fedca_proqc/ "Same disk: VM part. to VM part."
+./benchmark-mtran.sh /media/master/github/opwr          /media/imdb/                "Same HD: VM part. to VM part."
+./benchmark-mtran.sh /media/master/apps/                /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
+./benchmark-mtran.sh /media/master/mywiki_deprecated/   /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
+./benchmark-mtran.sh /media/master/github/fedca_proqc/  /media/imdb/                "Same HD: VM part. to VM part."
 
-./benchmark-mtran.sh /media/master/github/opwr /media/master/github/fedca_proqc/ "Same disk: VM part. to VM part."
-./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
-./benchmark-mtran.sh /media/master/github/opwr /media/imdb/ "Same disk: VM part. to VM part."
-./benchmark-mtran.sh /media/master/mywiki_deprecated/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
+./benchmark-mtran.sh /media/master/github/fedca_proqc/  /media/imdb/                "Same HD: VM part. to VM part."
+./benchmark-mtran.sh /media/master/apps/                /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
+./benchmark-mtran.sh /media/master/github/opwr          /media/imdb/                "Same HD: VM part. to VM part."
+./benchmark-mtran.sh /media/master/mywiki_deprecated/   /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
 
-./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
-./benchmark-mtran.sh /media/master/mywiki_deprecated/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
-./benchmark-mtran.sh /media/master/github/opwr /media/master/github/fedca_proqc/ "Same disk: VM part. to VM part."
-./benchmark-mtran.sh /media/master/github/opwr /media/imdb/ "Same disk: VM part. to VM part."
+./benchmark-mtran.sh /media/master/apps/                /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
+./benchmark-mtran.sh /media/master/mywiki_deprecated/   /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
+./benchmark-mtran.sh /media/master/github/fedca_proqc/  /media/imdb/                "Same HD: VM part. to VM part."
+./benchmark-mtran.sh /media/master/github/opwr          /media/imdb/                "Same HD: VM part. to VM part."
 
-./benchmark-mtran.sh /media/master/mywiki_deprecated/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
-./benchmark-mtran.sh /media/master/github/opwr /media/imdb/ "Same disk: VM part. to VM part."
-./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
-./benchmark-mtran.sh /media/master/github/opwr /media/master/github/fedca_proqc/ "Same disk: VM part. to VM part."
+./benchmark-mtran.sh /media/master/mywiki_deprecated/   /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
+./benchmark-mtran.sh /media/master/github/opwr          /media/imdb/                "Same HD: VM part. to VM part."
+./benchmark-mtran.sh /media/master/apps/                /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
+./benchmark-mtran.sh /media/master/github/fedca_proqc/  /media/imdb/                "Same HD: VM part. to VM part."
 
-./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
-./benchmark-mtran.sh /media/master/github/opwr /media/imdb/ "Same disk: VM part. to VM part."
-./benchmark-mtran.sh /media/master/mywiki_deprecated/ /media/sf_shared/test-data/ "Same disk: VM HD to shared dir"
-./benchmark-mtran.sh /media/master/github/opwr /media/master/github/fedca_proqc/ "Same disk: VM part. to VM part."
+./benchmark-mtran.sh /media/master/apps/                /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
+./benchmark-mtran.sh /media/master/github/opwr          /media/imdb/                "Same HD: VM part. to VM part."
+./benchmark-mtran.sh /media/master/mywiki_deprecated/   /media/sf_shared/test-data/ "Same HD: VM HD to shared dir"
+./benchmark-mtran.sh /media/master/github/fedca_proqc/  /media/imdb/                "Same HD: VM part. to VM part."
 
 ./benchmark-graph.sh
 ./benchmark-dataset-group.sh  "${LOGFILE_DIR}"
