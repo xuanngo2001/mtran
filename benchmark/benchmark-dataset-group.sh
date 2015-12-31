9daa0e07f89b271b86e72fd332b442095f986e86
limelime <limelime@users.noreply.github.com>
2015-12-31 13:46:14 -0500

    Dataset is now grouped. Hence, better graph.

---
+++ benchmark/benchmark-dataset-group.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+# Description: Group data per test case description & dataset
+
+LOGFILE_DIR=$1
+
+if [ ! -d "${LOGFILE_DIR}" ]; then echo "Error: Log file directory: ${LOGFILE_DIR}: no such directory. Aborted!"; exit 1; fi
+
+LOGFILE_DIR=$(readlink -ev "${LOGFILE_DIR}")
+
+while IFS='' read -r LOGFILE || [[ -n "$LOGFILE" ]]; do    
+  echo "$LOGFILE"
+  sort -t ';' -k2,2 -k3,3 "${LOGFILE}" -o "${LOGFILE}"
+done < <( find "${LOGFILE_DIR}" -type f -name "*.log" )
+ 
+
--- benchmark/run-ben.sh
+++ benchmark/run-ben.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
+LOGFILE_DIR=./tmp-working
 
 ./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk, diff partition"
 ./benchmark-mtran.sh /media/master/mywiki_deprecated/ /media/sf_shared/test-data/ "Same disk, diff partition"
 
 ./benchmark-graph.sh
-# Execute gnuplot.
+./benchmark-dataset-group.sh  "${LOGFILE_DIR}"
 ./benchmark-graph-gnuplot.pg
 ./benchmark-results.sh
