0433e47098dfb1dd01e31240ee23b2a3851077ce
limelime <limelime@users.noreply.github.com>
2015-12-31 12:36:57 -0500

    update.

--- benchmark/run-ben.sh
+++ benchmark/run-ben.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-#./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk, diff partition"
+./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk, diff partition"
 ./benchmark-mtran.sh /media/master/mywiki_deprecated/ /media/sf_shared/test-data/ "Same disk, diff partition"
 
 ./benchmark-graph.sh
