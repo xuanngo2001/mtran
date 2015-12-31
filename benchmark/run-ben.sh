47b0e9058103228be6385cd795fe2dc174667bdb
limelime <limelime@users.noreply.github.com>
2015-12-31 14:48:21 -0500

    update.

--- benchmark/run-ben.sh
+++ benchmark/run-ben.sh
@@ -3,8 +3,14 @@ set -e
 
 LOGFILE_DIR=./tmp-working
 
-./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk, diff partition"
-./benchmark-mtran.sh /media/master/mywiki_deprecated/ /media/sf_shared/test-data/ "Same disk, diff partition"
+# 257M
+#./benchmark-mtran.sh /media/master/apps/ /media/sf_shared/test-data/ "Same disk, diff partition"
+
+# 500M
+#./benchmark-mtran.sh /media/master/mywiki_deprecated/ /media/sf_shared/test-data/ "Same disk, diff partition"
+
+# 803M
+./benchmark-mtran.sh /media/master/github/opwr /media/sf_shared/test-data/ "Same disk, diff partition"
 
 ./benchmark-graph.sh
 ./benchmark-dataset-group.sh  "${LOGFILE_DIR}"
