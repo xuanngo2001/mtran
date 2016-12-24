54a998ba4cbe7b80160f850b26d55cd22b3f4d36
limelime <limelime@users.noreply.github.com>
2016-12-23 22:09:00 -0500

    Update benchmark-results.sh

--- benchmark/benchmark-results.sh
+++ benchmark/benchmark-results.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
-# Description: Extract command details from mtran.sh and add it benchmark-results.md.
+# Description: Extract command details from mtran.sh and add it readme.md.
 
 MTRAN_SCRIPT=$(whereis mtran | cut -d ' ' -f2)
 if [ -z "${MTRAN_SCRIPT}" ]; then echo "Error: mtran is not found. Aborted!"; exit 1; fi
 
 echo "${MTRAN_SCRIPT}"
 
-BENCHMARK_RESULTS_MD=benchmark-results.md
+BENCHMARK_RESULTS_MD=readme.md
 # Delete all lines after '# Commands code'
 sed -i '/# Commands code/,$d' "${BENCHMARK_RESULTS_MD}"
 echo '# Commands code' >> "${BENCHMARK_RESULTS_MD}"
