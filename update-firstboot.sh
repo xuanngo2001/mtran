70923828d3928cb80dd531395fe4a68aca24e6dd
limelime <limelime@users.noreply.github.com>
2017-04-21 10:18:56 -0400

    Improve description.

--- update-firstboot.sh
+++ update-firstboot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-# Description: Update mtran to firstboot project
+# Description: Update mtran.sh to firstboot project
 
 mtran_bin_dir=$(readlink -ev "/media/master/github/firstboot/apps/mtran/run/")
 yes | cp -v mtran.sh "${mtran_bin_dir}" 
