325ea61b080b4e02576ed06f1a30655512ede18c
limelime <limelime@users.noreply.github.com>
2018-11-26 00:40:42 +0000

    fix path.

--- update-firstboot.sh
+++ update-firstboot.sh
@@ -2,5 +2,5 @@
 set -e
 # Description: Update mtran.sh to firstboot project
 
-mtran_bin_dir=$(readlink -ev "/media/master/github/firstboot/apps/mtran/run/")
+mtran_bin_dir=$(readlink -ev "/media/master/github/firstboot/firstboot/scripts/xtra-app-mtran/run/")
 yes | cp -v mtran.sh "${mtran_bin_dir}" 
